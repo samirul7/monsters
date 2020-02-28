6456aff15c132eba6041c4245a4e3620bf8f040d
samirul7 <mdsamirulislam7@gmail.com>
2020-02-28 18:09:42 +0530

    upto signin done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 
-import HomePage from './homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import Header from './components/header/header.component'
 
 const App = () => (
   <div>
-    <HomePage />
+    <Header />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUp} />
+    </Switch>
   </div>
 )
 
