f3dcdfaf070de887289898e5171f1914fc342173
Md Samirul Islam <44937505+samirul7@users.noreply.github.com>
2020-03-01 22:36:26 +0530

    Update firebase.utils.js
    hide some data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: "AIzaSyDRAT_rxgYC2rIC7SHMoRP8QYx9buSrqQo",
-  authDomain: "sami-ecommerce.firebaseapp.com",
-  databaseURL: "https://sami-ecommerce.firebaseio.com",
-  projectId: "sami-ecommerce",
-  storageBucket: "sami-ecommerce.appspot.com",
-  messagingSenderId: "326765893009",
-  appId: "1:326765893009:web:6e219051aa735fa9387630",
-  measurementId: "G-4F6PJSSXY9"
-};
+const config = null;
 
 firebase.initializeApp(config);
 
